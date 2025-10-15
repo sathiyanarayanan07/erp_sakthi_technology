99eab3039e665fa6c9d35bd5f7bdc5928ff9efd8
sathiyanarayanan07 <sathiyanarayananr67@gmail.com>
2025-10-15 12:25:07 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     path("single_login/",views.single_login,name="role1_login"),
     path("get_user_data/",views.get_user_data,name="get_user_data"),
-    path("single_signup/",views.single_signup,name="single_signup"),
+    path("single_signup/",views.admin_single_signup,name="single_signup"),
+    path("logout/",views.logout,name="logout"),
     path("add_product_details/",views.add_product_details,name="add_product_details"),
-    path("get_product_details/",views.get_product_details,name="get_product_details"),
     path("update_product_status/<int:id>/",views.update_product_status,name="update_product_status"),
     path("delete_product/<int:id>/",views.delete_product,name="delete_product"),
     path("qa_view/",views.qa_view,name="qa_view"),
@@ -18,6 +18,8 @@ urlpatterns = [
     path("get_product_material/",views.get_product_materials,name="get_product_material"),
     path("add_product_options/",views.add_product_options,name="add_product_options"),
     path("get_product_options/",views.get_product_options,name="get_product_options"),
+    path("Schedule_add/",views.Schedule_add,name="Schedule_add")
+   
    
 
   
