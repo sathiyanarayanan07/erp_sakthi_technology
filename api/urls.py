2c9f2c8f51021b2e176749b38d4f8e1c32838f65
sathiyanarayanan07 <sathiyanarayananr67@gmail.com>
2025-10-15 15:28:59 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -20,7 +20,9 @@ urlpatterns = [
     path("get_product_options/",views.get_product_options,name="get_product_options"),
     path("Schedule_add/",views.Schedule_add,name="Schedule_add"),
     path("Schedule_view/",views.Schedule_view,name="Schedule_view"),
-     path("product_qa_view/",views.product_qa_view,name="product_qa_view")
+    path("product_qa_view/",views.product_qa_view,name="product_qa_view"),
+    path("add_account/",views.add_account,name="create_account")
+   
    
    
 
